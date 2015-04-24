0efcc88461a41fb34c58bfce29a484c93e90dac0
David Moore <dm21953@gmail.com>
2015-04-24 12:41:32 -0400

    modified 4/24

--- convolutional_mlp.py
+++ convolutional_mlp.py
@@ -36,6 +36,7 @@ from load_data import load_data
 from logistic_sgd import LogisticRegression
 from mlp import HiddenLayer
 
+print_on = False
 
 class LeNetConvPoolLayer(object):
     """Pool Layer of a convolutional network """
@@ -63,6 +64,8 @@ class LeNetConvPoolLayer(object):
         """
 
         assert image_shape[1] == filter_shape[1]
+	if print_on:
+		input = theano.printing.Print("input = ")(input)
         self.input = input
 
         # there are "num input feature maps * filter height * filter width"
@@ -94,8 +97,11 @@ class LeNetConvPoolLayer(object):
             filter_shape=filter_shape,
             image_shape=image_shape
         )
+	if print_on:
+		conv_out = theano.printing.Print("conv_out = ")(conv_out)
 
         # downsample each feature map individually, using maxpooling
+	print poolsize
         pooled_out = downsample.max_pool_2d(
             input=conv_out,
             ds=poolsize,
@@ -106,15 +112,20 @@ class LeNetConvPoolLayer(object):
         # reshape it to a tensor of shape (1, n_filters, 1, 1). Each bias will
         # thus be broadcasted across mini-batches and feature map
         # width & height
+	if print_on:
+		pooled_out = theano.printing.Print("pooled_out = ")(pooled_out)
         self.output = T.tanh(pooled_out + self.b.dimshuffle('x', 0, 'x', 'x'))
 
         # store parameters of this layer
         self.params = [self.W, self.b]
 
+"""
+Change from MNIST dataset to regular dataset
+"""
 
 def evaluate_lenet5(learning_rate=0.1, n_epochs=200,
                     dataset='mnist.pkl.gz',
-                    nkerns=[20, 50], batch_size=500):
+                    nkerns=[20, 50], batch_size=10):
     """ Demonstrates lenet on MNIST dataset
 
     :type learning_rate: float
@@ -133,17 +144,11 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=200,
 
     rng = numpy.random.RandomState(23455)
 
-    datasets = load_data(dataset)
+    datasets = load_data()
 
-    train_set = datasets[0]
-    train_set_x = train_set.ix[:,0:(train_set.shape[1]-1)]
-    train_set_y = train_set.ix[:,train_set.shape[1]-1]
-    valid_set = datasets[1]
-    valid_set_x = train_set.ix[:,0:(valid_set.shape[1]-1)]
-    valid_set_y = train_set.ix[:,valid_set.shape[1]-1]
-    test_set = datasets[2]
-    test_set_x = train_set.ix[:,0:(test_set.shape[1]-1)]
-    test_set_y = train_set.ix[:,test_set.shape[1]-1]
+    train_set_x, train_set_y = datasets[0]
+    valid_set_x, valid_set_y = datasets[1]
+    test_set_x, test_set_y = datasets[2]
 
     # compute number of minibatches for training, validation and testing
     n_train_batches = train_set_x.get_value(borrow=True).shape[0]
@@ -169,31 +174,48 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=200,
     # Reshape matrix of rasterized images of shape (batch_size, 28 * 28)
     # to a 4D tensor, compatible with our LeNetConvPoolLayer
     # (28, 28) is the size of MNIST images.
-    layer0_input = x.reshape((batch_size, 1, 28, 28))
+    
+    #Change shape here
+    layer0_input = x.reshape((batch_size, 1, 14, 56))
 
     # Construct the first convolutional pooling layer:
     # filtering reduces the image size to (28-5+1 , 28-5+1) = (24, 24)
     # maxpooling reduces this further to (24/2, 24/2) = (12, 12)
     # 4D output tensor is thus of shape (batch_size, nkerns[0], 12, 12)
+
+    # Construct the first convolutional pooling layer:
+    # filtering reduces the image size to (14-5+1 , 56-5+1) = (10, 52)
+    # maxpooling reduces this further to (10, 52/2) = (10, 26)
+    # 4D output tensor is thus of shape (batch_size, nkerns[0], 5, 26)
     layer0 = LeNetConvPoolLayer(
         rng,
         input=layer0_input,
-        image_shape=(batch_size, 1, 28, 28),
+        image_shape=(batch_size, 1, 14, 56),
         filter_shape=(nkerns[0], 1, 5, 5),
-        poolsize=(2, 2)
+        poolsize=(1, 2)
     )
 
     # Construct the second convolutional pooling layer
     # filtering reduces the image size to (12-5+1, 12-5+1) = (8, 8)
     # maxpooling reduces this further to (8/2, 8/2) = (4, 4)
     # 4D output tensor is thus of shape (batch_size, nkerns[1], 4, 4)
+
+    # Construct the second convolutional pooling layer
+    # filtering reduces the image size to (10-5+1, 26-5+1) = (1, 22)
+    # maxpooling reduces this further to (1, 22/2) = (1, 11)
+    # 4D output tensor is thus of shape (batch_size, nkerns[1], 1, 11)
     layer1 = LeNetConvPoolLayer(
         rng,
         input=layer0.output,
-        image_shape=(batch_size, nkerns[0], 12, 12),
+        image_shape=(batch_size, nkerns[0], 10, 26),
         filter_shape=(nkerns[1], nkerns[0], 5, 5),
         poolsize=(2, 2)
     )
+
+    # the HiddenLayer being fully-connected, it operates on 2D matrices of
+    # shape (batch_size, num_pixels) (i.e matrix of rasterized images).
+    # This will generate a matrix of shape (batch_size, nkerns[1] * 4 * 4),
+    # or (500, 50 * 4 * 4) = (500, 800) with the default values.
 
     # the HiddenLayer being fully-connected, it operates on 2D matrices of
     # shape (batch_size, num_pixels) (i.e matrix of rasterized images).
@@ -205,13 +227,13 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=200,
     layer2 = HiddenLayer(
         rng,
         input=layer2_input,
-        n_in=nkerns[1] * 4 * 4,
-        n_out=500,
+        n_in=1650,#nkerns[1] * 4 * 4,
+        n_out=1650,
         activation=T.tanh
     )
 
     # classify the values of the fully-connected sigmoidal layer
-    layer3 = LogisticRegression(input=layer2.output, n_in=500, n_out=10)
+    layer3 = LogisticRegression(input=layer2.output, n_in=1650, n_out=2)
 
     # the cost we minimize during training is the NLL of the model
     cost = layer3.negative_log_likelihood(y)
--- load_data.py
+++ load_data.py
@@ -3,6 +3,9 @@ import numpy as np
 from operator import mul
 from functools import partial
 import os
+import theano
+import numpy
+import theano.tensor as T
 
 def load_data():
     # load positive data and resize to add label
@@ -15,13 +18,21 @@ def load_data():
     train = np.concatenate((positive.ix[:,1:np.size(positive,axis=1)],negative.ix[:,1:np.size(negative,axis=1)]),axis=1) # combine them except the first column
     train = train.T
     train = pd.DataFrame(train) # Now convert it to a data frame
-    train['label'] = 0 # Now add in a label column initially set to all 0's
-    train.loc[0:94,'label'] = 1 # Now set the first 94 samples to 1 to indicate they are positive
-    
+    train['label'] = 0
     train.columns = ["X"+str(x) for x in list(ppm)] + ['label'] # Now correct the column names
-
-    random_order = np.random.permutation(np.size(train,axis=0)) #sample(1:nrow(features),nrow(features))
+    #train.ix[:,train.shape[1]-1] = 0
+    train.ix[0:94,train.shape[1]-1] = 1 # Now set the first 94 samples to 1 to indicate they are positive
+    #print train.ix[0:94,train.shape[1]-1]
+    
+    #train = pd.DataFrame(train.ix[0:200,:])
+    #print train
+    #print train.shape
+    random_order = np.random.permutation(train.shape[0]) #sample(1:nrow(features),nrow(features))
+    #print random_order
     train = train.ix[random_order,:]
+    #print train.ix[0:94,train.shape[1]-1]
+    #lkajdsf = ljdf
+    
 
     # count rows and dims
     n = train.shape[0]
@@ -32,13 +43,47 @@ def load_data():
                                                   map(partial(mul, n),
                                                       (train_frac, test_frac,
                                                        valid_frac)))
-    train_set, test_set, valid_set = (train[0:train_offset],
+    train_set, test_set, valid_set = (train[0:train_offset].as_matrix(),
                                       train[train_offset+1:
-                                               train_offset+1+test_offset],
+                                               train_offset+1+test_offset].as_matrix(),
                                       train[train_offset+1+test_offset+1:
-                                               train_offset+1+test_offset+1+valid_offset])
+                                               train_offset+1+test_offset+1+valid_offset].as_matrix())
 
-    return train_set, valid_set, test_set
+    def shared_dataset(data_xy, borrow=True):
+        """ Function that loads the dataset into shared variables
+        The reason we store our dataset in shared variables is to allow
+        Theano to copy it into the GPU memory (when code is run on GPU).
+        Since copying data into the GPU is slow, copying a minibatch everytime
+        is needed (the default behaviour if the data is not in a shared
+        variable) would lead to a large decrease in performance.
+        """
+        #data_x, data_y = data_xy[:,0:data_xy.shape[1]-1],data_xy[:,data_xy.shape[1]-1]
+        data_x, data_y = data_xy[:,0:784],data_xy[:,data_xy.shape[1]-1]
+	print data_y
+	#data_x = data_x.todense()
+	#print type(data_x)# == numpy.ndarray
+        shared_x = theano.shared(numpy.asarray(data_x,
+                                               dtype=theano.config.floatX),
+                                 borrow=borrow)
+        shared_y = theano.shared(numpy.asarray(data_y,
+                                               dtype=theano.config.floatX),
+                                 borrow=borrow)
+        # When storing data on the GPU it has to be stored as floats
+        # therefore we will store the labels as ``floatX`` as well
+        # (``shared_y`` does exactly that). But during our computations
+        # we need them as ints (we use labels as index, and if they are
+        # floats it doesn't make sense) therefore instead of returning
+        # ``shared_y`` we will have to cast it to int. This little hack
+        # lets ous get around this issue
+        return shared_x, T.cast(shared_y, 'int32')
+
+    test_set_x, test_set_y = shared_dataset(test_set)
+    valid_set_x, valid_set_y = shared_dataset(valid_set)
+    train_set_x, train_set_y = shared_dataset(train_set)
+
+    rval = [(train_set_x, train_set_y), (valid_set_x, valid_set_y),
+            (test_set_x, test_set_y)]
+    return rval
 
 
 def test_load_data():
